023d43566ceee8f1f0623a9d6148e365e5b11c4b
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-10-02 22:42:17 -0300

    Add files via upload

---
+++ C#/Classes_atributos_metodos/Retangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade {
+    internal class Program {
+        static void Main(string[] args) {
+            Retangulo R = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retangulo: ");
+            R.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            R.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"AREA = {R.Area().ToString("F2", CultureInfo.InvariantCulture)}\nPERÍMETRO = {R.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}\nDIAGONAL = {R.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+            
+        }
+    }
+}
