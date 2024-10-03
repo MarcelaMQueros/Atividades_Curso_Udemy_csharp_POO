da2d4a5ed650997d400bef0fe30a8aa606dac7ac
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-10-03 16:48:19 -0300

    Add files via upload

---
+++ C#/Membros_estáticos/Conversor_de_moeda/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade {
+    public static class Conversor {
+
+        public static int IOF = 6;
+
+        public static double ValorPagar(double cota, double qnt) {
+            return (cota * qnt) + (((cota * qnt) * IOF) / 100);
+        }
+
+    }
+}
---
+++ C#/Membros_estáticos/Conversor_de_moeda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual a cotação do dólar? ");
+            double cota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qnt = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = Conversor.ValorPagar(cota, qnt);
+
+            Console.WriteLine($"Valor a ser pago em reais = {resultado.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
