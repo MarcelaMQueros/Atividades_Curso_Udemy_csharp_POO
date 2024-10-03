003a69c02b0642aceeaa543f57f56908b07073bf
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-10-02 23:21:43 -0300

    Add files via upload

---
+++ C#/Classes_atributos_metodos/Funcionario/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Atividade {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salario bruto:");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine($"Funcionario: {funcionario}");
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            funcionario.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.Write($"Dados atualizados: {funcionario}");
+        }
+    }
+}
